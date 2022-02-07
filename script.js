92c1cac7ced5d949936e675addf26c868e8a4061
krasotun <servrostov@gmail.com>
2022-02-07 21:50:39 +0300

    add исправил все замечания ревьювера

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
-// Работа с поп-апом
-let buttonEditInfo = document.querySelector('.profile__edit-button');
-let buttonClosePopup = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
+// Переменные для работы с   поп-апом
+const buttonEditInfo = document.querySelector('.profile__edit-button');
+const buttonClosePopup = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
 
+// Переменные для работы с формой
+const profileTitleText = document.querySelector('.profile__title');
+const profileSubTitleText = document.querySelector('.profile__subtitle');
+
+// Находим форму в DOM
+const formElement = document.querySelector('.form');
+
+// Находим поля формы в DOM
+const nameInput = formElement.querySelector('#name');
+const jobInput = formElement.querySelector('#job');
+
+// Объявляем функцию для открытия поп-апа
 function showPopUp() {
-  popup.classList.add("popup_opened");
+  popup.classList.add("popup_opened"); // Показываем поп-ап
+  letDefaultVariables(); // Заполняем дефолтные значения
 }
 
+// Объявляем функцию для подстановки изначальных значений в поля формы
+function letDefaultVariables() {
+  nameInput.value = profileTitleText.textContent;
+  jobInput.value = profileSubTitleText.textContent;
+}
+
+// Объявляем функцию для закрытия поп-апа
 function hidePopUp() {
   popup.classList.remove("popup_opened");
 }
 
-
+// Событие открытие поп-апа
 buttonEditInfo.addEventListener('click', showPopUp);
+
+// Событие закрытие поп-апа
 buttonClosePopup.addEventListener('click', hidePopUp);
 
 // Работа с формой
 
-// Находим форму в DOM
-let formElement = document.querySelector('form');
-
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('#name');
-let jobInput = formElement.querySelector('#job');
-
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
-  let nameInputValue = nameInput.value;
-  let jobInputValue = jobInput.value;
-
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let profileTitleText = document.querySelector('.profile__title');
-  let profileSubTitleText = document.querySelector('.profile__subtitle');
-
-  // Вставьте новые значения с помощью textContent
-  profileTitleText.textContent = nameInputValue;
-  profileSubTitleText.textContent = jobInputValue;
+// Обработчик «отправки» формы
+function editAccountInfo(evt) {
+  evt.preventDefault(); //
+  profileTitleText.textContent = nameInput.value;
+  profileSubTitleText.textContent = jobInput.value;
   hidePopUp();
 }
 
 // Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', editAccountInfo);
