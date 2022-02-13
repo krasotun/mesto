4452e063600fc3af5b44d90ab826cf57151a49f6
Marat <servrostov@gmail.com>
2022-02-13 13:51:58 +0300

    add Сделал поп-ап для добавления карточки

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const buttonEditInfo = document.querySelector('.profile__edit-button');
 const buttonClosePopup = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
 
+// Переменные для работы с поп-апом добавления карточки
+const buttonAddNewCard = document.querySelector('.profile__post-button');
+const buttonClosePopupAdd = document.querySelector('.popup-add__close');
+const popupAdd = document.querySelector('.popup-add');
+
 // Переменные для работы с формой отправки информации
 const profileTitleText = document.querySelector('.profile__title');
 const profileSubTitleText = document.querySelector('.profile__subtitle');
@@ -31,14 +36,13 @@ const removeAllCards = () => {
   });
 };
 
-// Функция для создания  одной карточки
-const createCard = () => {
+// Функция для создания   карточки из формы
+const createCardFromForm = () => {
   const newCardFromTemplate = cardTemplate.querySelector('.elements__card-container').cloneNode(true);
   newCardFromTemplate.querySelector('.elements__image').src =
     'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
   newCardFromTemplate.querySelector('.elements__image').alt = 'Альт картинки';
   newCardFromTemplate.querySelector('.elements__text').textContent = 'Название';
-
   elements.append(newCardFromTemplate);
 };
 
@@ -56,11 +60,18 @@ const firstAddCards = () => {
 };
 firstAddCards();
 
-// Объявляем функцию для открытия поп-апа
+// Объявляем функцию для открытия поп-апа редактированая информации
 function showPopUp() {
   popup.classList.add("popup_opened"); // Показываем поп-ап
   letDefaultVariables(); // Заполняем дефолтные значения
 }
+
+// Объявляем функцию для открытия поп-апа добавления новой карточки
+function showPopUpAdd() {
+  popupAdd.classList.add("popup-add_opened"); // Показываем поп-ап
+}
+
+
 
 // Объявляем функцию для подстановки изначальных значений в поля формы
 function letDefaultVariables() {
@@ -68,16 +79,31 @@ function letDefaultVariables() {
   jobInput.value = profileSubTitleText.textContent;
 }
 
-// Объявляем функцию для закрытия поп-апа
+// Объявляем функцию для закрытия поп-апа редактирования информации
 function hidePopUp() {
   popup.classList.remove("popup_opened");
 }
 
-// Событие открытие поп-апа
+// Объявляем функцию для закрытия поп-апа добавления новой карточки
+function hidePopUpAdd() {
+  popupAdd.classList.remove("popup-add_opened");
+}
+
+// Событие открытие поп-апа редактирования информации
 buttonEditInfo.addEventListener('click', showPopUp);
 
-// Событие закрытие поп-апа
+// Событие открытие поп-апа добавление новой карточки
+buttonAddNewCard.addEventListener('click', showPopUpAdd);
+
+
+
+// Событие закрытие поп-апа редактирования информации
 buttonClosePopup.addEventListener('click', hidePopUp);
+
+// Событие закрытие поп-апа добавление новой карточки
+buttonClosePopupAdd.addEventListener('click', hidePopUpAdd);
+
+
 
 // Работа с формой
 
