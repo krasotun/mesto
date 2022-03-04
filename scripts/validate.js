88f2fde79d3ab7143c784b60dad925cc9ae667ef
Marat <servrostov@gmail.com>
2022-03-04 16:05:04 +0300

    add Стилизовал сообщения об ошибках

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,2 +1,15 @@
 const formEditItem = formEditElement.querySelectorAll('.form__item');
 console.log(formEditItem);
+
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+const enableValidation = ({
+  formSelector: '.form',
+  inputSelector: '.form__item"',
+  submitButtonSelector: '.form__submit-button',
+  inactiveButtonClass: 'form__submit-button_inactive',
+  inputErrorClass: 'form__item_type_error',
+  errorClass: 'form__error_visible'
+});
