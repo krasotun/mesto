aa226bce8019749264ca305ca3b2d9acfa195e9d
Marat <servrostov@gmail.com>
2022-03-04 17:05:53 +0300

    add Добавил обработчики событий на поля всех форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,15 +1,27 @@
-const formEditItem = formEditElement.querySelectorAll('.form__item');
-console.log(formEditItem);
-
-
-// включение валидации вызовом enableValidation
-// все настройки передаются при вызове
-
-const enableValidation = ({
+const obj = {
   formSelector: '.form',
-  inputSelector: '.form__item"',
+  inputSelector: '.form__item',
   submitButtonSelector: '.form__submit-button',
   inactiveButtonClass: 'form__submit-button_inactive',
   inputErrorClass: 'form__item_type_error',
   errorClass: 'form__error_visible'
-});
+};
+
+function enableValidation(obj) {
+  const forms = Array.from(document.querySelectorAll(obj.formSelector)); // Выбираем все формы
+  forms.forEach(form => { // Перебираем  формы
+    const inputs = form.querySelectorAll(obj.inputSelector); //Выбираем все инпуты
+    inputs.forEach(input => {
+      addEventListenersForInputs(input);
+    });
+  });
+
+}
+
+
+// Наложение обработчиков на поля форм
+const addEventListenersForInputs = (input) => {
+  input.addEventListener('input', evt => console.log(evt));
+};
+
+enableValidation(obj);
