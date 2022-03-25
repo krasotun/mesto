f2e4c948792841bd0b841bb4e251360ec6a5d866
Marat <servrostov@gmail.com>
2022-03-25 15:04:44 +0300

    fix Исправил названия переменных в валидаторе

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,16 +1,16 @@
 export class FormValidator {
   constructor(obj, formName) {
     this.formName = formName;
-    this.formSelector = obj.formSelector;
-    this.inputSelector = obj.inputSelector;
-    this.submitButtonSelector = obj.submitButtonSelector;
-    this.inactiveButtonClass = obj.inactiveButtonClass;
-    this.inputErrorClass = obj.inputErrorClass;
-    this.errorClass = obj.errorClass;
+    this._formSelector = obj.formSelector;
+    this._inputSelector = obj.inputSelector;
+    this._submitButtonSelector = obj.submitButtonSelector;
+    this._inactiveButtonClass = obj.inactiveButtonClass;
+    this._input_errorClass = obj.input_errorClass;
+    this._errorClass = obj.errorClass;
   }
   _setEventListeners() { // Устанавливаем обработчики событий
-    const inputList = Array.from(this.formName.querySelectorAll(this.inputSelector));
-    const submitButton = this.formName.querySelector(this.submitButtonSelector);
+    const inputList = Array.from(this.formName.querySelectorAll(this._inputSelector));
+    const submitButton = this.formName.querySelector(this._submitButtonSelector);
     inputList.forEach(inputElement => {
       inputElement.addEventListener('input', () => {
         this._checkInputValidity(inputElement); // проверяем инпут
@@ -28,13 +28,13 @@ export class FormValidator {
   _showInputError(inputElement) { // Показываем ошибку для невалидного инпута
     const formInputElementName = inputElement.getAttribute('name');
     const errorName = document.getElementById(`${formInputElementName}-error`);
-    errorName.classList.add(this.errorClass);
+    errorName.classList.add(this._errorClass);
     errorName.textContent = inputElement.validationMessage;
   }
   _hideInputError(inputElement) { // Скрываем ошибку у валидного инпута
     const formInputElementName = inputElement.getAttribute('name');
     const errorName = document.getElementById(`${formInputElementName}-error`);
-    errorName.classList.remove(this.errorClass);
+    errorName.classList.remove(this._errorClass);
     errorName.textContent = '';
   }
   _hasInvalidInput(inputList) { // Проверяем валмидность у всех инпутов
@@ -44,10 +44,10 @@ export class FormValidator {
   }
   _toggleButtonState(inputList, submitButton) { // Переключаем состояние кнопки сабмита
     if (this._hasInvalidInput(inputList)) {
-      submitButton.classList.add(this.inactiveButtonClass);
+      submitButton.classList.add(this._inactiveButtonClass);
       submitButton.setAttribute('disabled', true);
     } else {
-      submitButton.classList.remove(this.inactiveButtonClass);
+      submitButton.classList.remove(this._inactiveButtonClass);
       submitButton.removeAttribute('disabled');
     }
   }
