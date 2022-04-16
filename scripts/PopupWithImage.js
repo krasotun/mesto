45bc93cba33e32109c405a07a358adead2970ef9
Marat <servrostov@gmail.com>
2022-04-16 17:09:44 +0300

    add Дописала PopupWithImage

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -3,6 +3,13 @@ import { Popup } from "./Popup";
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this.popupCardImage = popupSelector.querySelector('.popup-card__image');
+    this.popupCardText = popupSelector.querySelector('.popup-card__text');
   }
-
+  open(title, source) {
+    this.popupCardImage.src = source;
+    this.popupCardImage.alt = title;
+    this.popupCardText.textContent = title;
+    super.open();
+  }
 }
